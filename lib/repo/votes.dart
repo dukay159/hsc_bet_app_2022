01e13b62ecc766ca0f6199e74a1f60ada3a07f4a
dung172 <dvdung172@gmail.com>
2022-11-20 19:50:59 +0700

    update code

--- lib/repo/votes.dart
+++ lib/repo/votes.dart
@@ -1,48 +1,78 @@
+import 'dart:async';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'package:bet_app/model/appModel.dart';
+import 'package:flutter/material.dart';
 
 class DataRepository {
   // 1
   final CollectionReference collection =
-  FirebaseFirestore.instance.collection('11222');
+      FirebaseFirestore.instance.collection('demo');
+
   // 2
-  Stream<QuerySnapshot> getStream() {
-    return collection.snapshots();
+  Future<QuerySnapshot> getMatches() {
+    var timestamp = Timestamp.fromMillisecondsSinceEpoch(DateTime.now().add(const Duration(hours: 1)).millisecondsSinceEpoch);
+    // return collection.where('time' , isEqualTo : '2').snapshots();
+    return collection .orderBy('timematches', descending: false).startAt([timestamp]).get();
   }
+
+  Stream<DocumentSnapshot> getVote({required String id}) {
+    return collection
+        .doc(id)
+        .collection('votes')
+        .doc('dung@gmail.com')
+        .snapshots();
+  }
+
   // 3
   // Future<DocumentReference> addVote(Votes votes) {
   //   return collection.add(votes.toJson());
   // }
-  Future<void> addUser() {
+  Future<void> addUser({
+    required String id,
+    required String vote,
+  }) {
     // Call the user's CollectionReference to add a new user
-    return collection.doc('123')
+    return collection
+        .doc(id)
+        .collection('votes')
+        .doc('dung@gmail.com')
         .set({
-      'id': 1, // John Doe
-      'userId': 1, // Stokes and Sons
-      'vote': 1,
-      'matchTime': DateTime.now(),
-      'timeVote': DateTime.now()// 42
-    })
+          'vote': vote,
+          'timeVote': DateTime.now() // 42
+        })
         .then((value) => print("User Added"))
         .catchError((error) => print("Failed to add user: $error"));
   }
+
   // 4
   // void updateVote(Votes vote) async {
   //   await collection.doc('${vote.id}').update(vote.toJson());
   // }
-  Future<void> updateUser() {
+  Future<void> updateVote({
+    required String id,
+    required int vote,
+  }) {
     return collection
-        .doc('123')
-        .update({'id': 2})
-        .then((value) => print("User Updated"))
+        .doc(id)
+        .collection('votes')
+        .doc('dung@gmail.com')
+        .update({
+          'vote': vote,
+          'timevote': DateTime.now() // 42
+        })
+        .then((value) => print("User Update"
+            "d"))
         .catchError((error) => print("Failed to update user: $error"));
   }
+
   // 5
   void deleteVote(String id) async {
     // await collection.doc('${vote.id}').delete();
     await collection.doc('${id}').delete();
   }
+
   Future<void> batchDelete() async {
     WriteBatch batch = FirebaseFirestore.instance.batch();
 
